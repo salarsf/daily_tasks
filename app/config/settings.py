98dad66eb74d07e6b949ec7de09e0259757e59d3
Salar <salsaemi@gmail.com>
2022-10-09 17:29:23 +0330

    Task model created

--- app/config/settings.py
+++ app/config/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'rest_framework',
+
     # local apps
     'accounts',
+    'tasks',
 ]
 
 MIDDLEWARE = [
---
+++ app/tasks/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class Task(models.Model):
+    body = models.TextField()
+    date = models.DateField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        name = f"{self.user}-{self.date}-{self.body[:10]}..."
+        return name
