b9a6f0196b3832de2860e7cf9601b6cc73c7fb7a
Salar <salsaemi@gmail.com>
2022-10-29 17:38:43 +0330

    Added configurations for celery and redis

--- app/config/settings.py
+++ app/config/settings.py
@@ -135,6 +135,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Authentication settings
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'api:task_list'
+
+# Celery settings
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
